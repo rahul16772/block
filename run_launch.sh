f5d2206c27e5baa79071713eeae11bb44801f5fe
BrianW <14333653+bjw-0@users.noreply.github.com>
2025-07-26 23:54:03 -0600

    Merge pull request #56 from gensyn-ai/sr-minimize
    GitOrigin-RevId: ce2a556b7cafabda5ecd77c739b4e71f460b897a

--- run_launch.sh
+++ run_launch.sh
@@ -131,5 +131,17 @@ echo "=== BlockAssist Launch ==="
 # Set TMPDIR=/tmp on macOS to avoid path length errors
 if [[ "$OSTYPE" == "darwin"* ]]; then
     export TMPDIR=/tmp
+    PID=$(lsof -nP -iTCP:10001 | awk '$2 ~ /^[0-9]+$/ { print $2; exit }')
+
+    # Now tell System Events to minimize every window of that process by ID:
+    osascript <<EOF
+tell application "System Events"
+    tell (first process whose unix id is $PID)
+        repeat with w in windows
+            tell w to set value of attribute "AXMinimized" to true
+        end repeat
+    end tell
+end tell
+EOF
 fi
 python -m blockassist.launch $@
